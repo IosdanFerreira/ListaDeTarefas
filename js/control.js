9fc2a0392e7991a77b559b1966592c9e54c7c95a
IosdanFerreira <iosdan.ferreira@gmail.com>
2022-05-20 01:25:31 -0300

    Lista de tarefas/JavaScript

--- js/control.js
+++ js/control.js
@@ -1,6 +1,7 @@
 let input = document.getElementById("txtTarefas");
 let btnAdd = document.getElementById("button");
 let main = document.getElementById("areaLista");
+let contador = 0;
 
 //Configuando click do botão "Adicionar"
 function addTarefa(){
@@ -8,13 +9,14 @@ function addTarefa(){
 
     //Gerar no novo ítem
     if ((valorInput !== "") && (valorInput !== null) && (valorInput !== undefined)) {
-        let novoItem = `<div class="item" id="item">
-        <div class="item-icone">
-            <i class="mdi mdi-circle-outline"></i>
+        ++contador
+        let novoItem = `<div class="item" id="task-${contador}" id="item" >
+        <div class="item-icone" id="item-icone" onclick="marcarTarefa(${contador})">
+            <i id="icone-${contador}" class="mdi mdi-circle-outline"></i>
         </div>
-        <div class="item-nome">${valorInput}</div>
+        <div class="item-nome" onclick="marcarTarefa(${contador})">${valorInput}</div>
         <div class="item-botao">
-            <button class="delete" id="delete">
+            <button onclick="deletar(${contador})" class="btnDelete" id="btnDelete">
                 <i class="mdi mdi-delete"></i>
                 Deletar
             </button>
@@ -24,11 +26,16 @@ function addTarefa(){
     //Adicionar novo ítem no main
     main.innerHTML += novoItem
 
-    //Zerar input após o click
+    //Limpar input após o click
     input.value = "";
     input.focus();
 
     }
+}
+//Deletar tarefa
+function deletar(id) {
+    let tarefa = document.getElementById("task-"+id);
+    tarefa.remove();
 }
 //Se teclar ENTER
 input.addEventListener("keyup", function (event) {
@@ -36,3 +43,23 @@ input.addEventListener("keyup", function (event) {
         btnAdd.click()
     }
 })
+//Marcar e desmarcar tarefa feita
+function marcarTarefa(id){
+    let item = document.getElementById("task-"+id);
+    let classe = item.getAttribute('class');
+
+    if(classe == "item"){
+        item.classList.add('clicado');
+
+        var icone = document.getElementById("icone-"+id);
+        icone.classList.remove("mdi-circle-outline");
+        icone.classList.add("mdi-check-circle");
+        item.parentNode.appendChild(item)
+    }else{
+        var icone = document.getElementById("icone-"+id);
+        item.classList.remove('clicado')
+        item.classList.add('item');
+        icone.classList.remove("mdi-check-circle");
+        icone.classList.add("mdi-circle-outline");
+    }
+}
